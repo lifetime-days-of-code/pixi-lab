beae4c3866da138862db194ade392839c556e7c8
Plamen Mitev <44363976+o0pmitev@users.noreply.github.com>
2023-08-05 20:40:50 +0000

    fix: use ticker method

--- src/app.js
+++ src/app.js
@@ -2,7 +2,15 @@ import * as PIXI from 'pixi.js';
 import sample from './sample.png'
 
 let app = new PIXI.Application({width: 640, height: 360});
+let sprite = PIXI.Sprite.from(sample);
+let elapsed = 0.0;
 
 document.body.appendChild(app.view);
-let sprite = PIXI.Sprite.from(sample);
+
 app.stage.addChild(sprite);
+
+app.ticker.add((deltatime) => {
+    elapsed += deltatime;
+
+    sprite.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0;
+})
